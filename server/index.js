1e436896ad7f77e1cc5a53efb83459aa57985bcd
Alexstones <4lexstone11@gmail.com>
2026-06-16 21:34:22 -0600

    modificacion de dependencias

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-// server/index.js
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const path = require('path');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
@@ -14,6 +14,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Servir archivos estáticos del frontend (Vite)
+app.use(express.static(path.join(__dirname, '../dist')));
+
 const httpServer = http.createServer(app);
 initSocket(httpServer);
 
@@ -22,11 +25,18 @@ const server = new ApolloServer({ schema });
 
 (async () => {
   await server.start();
+  
   app.use('/graphql', expressMiddleware(server, {
     context: async ({ req }) => ({ req })
   }));
+
+  // Ruta para que React maneje la navegación
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  });
+
   const PORT = process.env.PORT || 4000;
   httpServer.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+    console.log(`🚀 Server ready at port ${PORT}`);
   });
 })();
